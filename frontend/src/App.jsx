449f9215821cf400966293b70f44e1a3b37a8236
Amoz19 <aungmon10co.com>
2023-11-19 17:21:05 +0630

    add:UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-zinc-900 w-2/6 rounded-md px-8 py-4">
+        <div className="flex justify-end">
+          <button className="bg-green-500 px-4 py-2 rounded ">1/3</button>
+        </div>
+
+        <div>
+          <h3 className="text-xl text-green-500">What is React?</h3>
+          <button className="flex w-full bg-white rounded my-4 px-4 py-2">
+            <p className="mr-4">1</p>
+            <p>Frontend Libary</p>
+          </button>
+          <button className="flex w-full bg-white rounded px-4 py-2">
+            <p className="mr-4">2</p>
+            <p>Backend Libary</p>
+          </button>
+        </div>
+        <div className="flex justify-between my-4  items-center">
+          <button className="bg-green-500 px-4 py-2 rounded">Next</button>
+          <p className="text-green-500">Correct Answer</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
